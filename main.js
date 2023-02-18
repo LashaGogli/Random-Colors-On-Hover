b517e9cb3d607939c7f1da9ddc16681e593ac017
Lasha <lashagoglidzelasha@gmail.com>
2023-02-18 12:04:10 +0400

    With Sound

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let charchoDiv = document.querySelector(".charchoDiv");
 let smallDivs;
 
+var x = document.getElementById("myAudio"); 
 
+function playAudio() { 
+  x.play(); 
+} 
 
+function pauseAudio() { 
+  x.pause(); 
+}
 
 for (let i = 0; i < 660; i++) {
     smallDivs = document.createElement("div");
@@ -20,6 +27,7 @@ boxes.forEach(box => {
         box.style.opacity = "100%";
         box.style.boxShadow = "0px 0px 30px 4px" + y;
         box.style.transitionDuration = "0s";
+        playAudio();
 
 
 
@@ -35,6 +43,7 @@ boxes.forEach(box => {
         box.style.transitionDuration = "2s";
         box.style.boxShadow = "none";
         box.style.opacity = "20%";
+        pauseAudio();
 
 
 
